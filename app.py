9736b5ef9274431a9a20e6527f9c540787756067
Sumit <sumit90asa@gmail.com>
2026-04-21 14:48:08 +0530

    fix homepage route

--- app.py
+++ app.py
@@ -225,4 +225,6 @@ def chatbot():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    @app.route("/")
+    def home():
+     return "CareConnect is running 🚀"
